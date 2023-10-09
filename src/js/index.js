9aabb4eb43d96857ae6c30db2dc72e5739a42238
MariiaSam <kontragent@ukr.net>
2023-10-09 11:47:28 +0300

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Report } from 'notiflix/build/notiflix-report-aio';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';import SimpleLightbox from 'simplelightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { createMarkup } from './markup';
 import { fetchImg } from './api';
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const btnLoad = (document.querySelector('.load-more').hidden = true);
 const markup = document.querySelector('.js-markup');
 
 const simpleLightbox = new SimpleLightbox('.gallery a');
@@ -16,19 +16,43 @@ form.addEventListener('submit', onSubmit);
 
 let searchValue;
 let page = 1;
+let errorTimeout;
+
+function showLoading() {
+  Loading.arrows();
+}
+
+function hideLoading() {
+  Loading.remove();
+}
+
+function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+function resetPage() {
+  page = 1;
+}
+
+function handleInvalidInput() {
+  Report.warning('Invalid input');
+  hideLoading();
+}
+
+function isValidInput(input) {
+  return /^[a-zA-Z0-9\s]+$/.test(input);
+}
 
 async function onSubmit(evt) {
   try {
     evt.preventDefault();
     observer.unobserve(markup);
-    gallery.innerHTML = '';
-    page = 1;
-    Loading.arrows();
+    clearGallery();
+    resetPage();
+    showLoading();
     searchValue = form.elements.searchQuery.value.trim();
-    const validInput = /^[a-zA-Z0-9\s]+$/.test(searchValue);
-    if (!validInput || searchValue === '') {
-      Report.warning('Invalid input');
-      Loading.remove();
+    if (!isValidInput(searchValue) || searchValue === '') {
+      handleInvalidInput();
       return;
     } else {
       const { hits, totalHits } = await fetchImg(searchValue);
@@ -41,15 +65,20 @@ async function onSubmit(evt) {
       evt.target.reset();
     }
   } catch (error) {
-    Report.warning('Please enter a valid search query.');
+    clearTimeout(errorTimeout);
+    Report.warning(`Error: ${error.message}`);
+    errorTimeout = setTimeout(() => {
+      Report.remove();
+    }, 3000);
+    // Report.warning('Please enter a valid search query.');
     console.log(error);
   } finally {
-    Loading.remove();
+    hideLoading();
   }
   observer.observe(markup);
 }
 
-function scrollUp() {
+function scrollToTop() {
   window.scrollTo({
     top: 0,
     behavior: 'smooth',
@@ -64,30 +93,29 @@ let options = {
 
 let observer = new IntersectionObserver(handlerPagination, options);
 
-async function handlerPagination(entries, observer) {
-    for (let entry of entries) {
-      if (entry.isIntersecting) {
-        try {
-          Loading.hourglass();
-          page += 1;
-          const { hits, totalHits } = await fetchImg(searchValue, page);
-          gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
-          simpleLightbox.refresh();
-          console.log(hits);
-          if (hits.length === 0 && entry.isIntersecting) {
-            Report.failure(
-              "We're sorry, but you've reached the end of search results."
-            );
-          }
-        } catch (err) {
-          console.log(err);
-  
+async function handlerPagination(entries) {
+  for (let entry of entries) {
+    if (entry.isIntersecting) {
+      try {
+        showLoading();
+        page += 1;
+        const { hits, totalHits } = await fetchImg(searchValue, page);
+        gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+        simpleLightbox.refresh();
+        console.log(hits);
+        if (hits.length === 0 && entry.isIntersecting) {
           Report.failure(
             "We're sorry, but you've reached the end of search results."
           );
-        } finally {
-          Loading.remove();
         }
+      } catch (error) {
+        clearTimeout(errorTimeout);
+        Report.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } finally {
+        hideLoading();
       }
     }
   }
+}
